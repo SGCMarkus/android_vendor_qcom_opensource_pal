3e3e7e7a165bb0df372ff6c8af5c11d8d5347666
Aditya Mohan <aditmoha@codeaurora.org>
2021-10-12 19:07:40 +0530

    pal: Change sequence of tear down of SP graphs
    In SP, mixer control is getting disabled first
    and then graph stop/close is happening which
    is leading to crash in DSP. Now disabling
    mixer controls after graph stop/close.
    
    Change-Id: I9f46f1ca5d6b4e09e9f61330fe01b5e7979e9f59

--- device/src/SpeakerProtection.cpp
+++ device/src/SpeakerProtection.cpp
@@ -877,20 +877,20 @@ err_pcm_open :
         if (status) {
             PAL_ERR(LOG_TAG, "Failed to deregister callback to rm");
         }
-        disableDevice(audioRoute, mSndDeviceName_vi);
         if (isTxStarted)
             pcm_stop(txPcm);
 
         pcm_close(txPcm);
+        disableDevice(audioRoute, mSndDeviceName_vi);
 
         txPcm = NULL;
     }
 
     if (rxPcm) {
-        disableDevice(audioRoute, mSndDeviceName_rx);
         if (isRxStarted)
             pcm_stop(rxPcm);
         pcm_close(rxPcm);
+        disableDevice(audioRoute, mSndDeviceName_rx);
         rxPcm = NULL;
     }
 
@@ -1701,7 +1701,6 @@ int32_t SpeakerProtection::spkrProtProcessingMode(bool flag)
         PAL_DBG(LOG_TAG, "pcm start for TX");
         if (pcm_start(txPcm) < 0) {
             PAL_ERR(LOG_TAG, "pcm start failed for TX path");
-            disableDevice(audioRoute, mSndDeviceName_vi);
             goto err_pcm_open;
         }
 
@@ -1728,7 +1727,6 @@ int32_t SpeakerProtection::spkrProtProcessingMode(bool flag)
             }
 
             ret = rm->getSndDeviceName(device.id , mSndDeviceName_vi);
-            disableDevice(audioRoute, mSndDeviceName_vi);
             rm->getBackendName(device.id, backEndName);
             if (!strlen(backEndName.c_str())) {
                 PAL_ERR(LOG_TAG, "Failed to obtain tx backend name for %d", device.id);
@@ -1736,6 +1734,7 @@ int32_t SpeakerProtection::spkrProtProcessingMode(bool flag)
             }
             pcm_stop(txPcm);
             pcm_close(txPcm);
+            disableDevice(audioRoute, mSndDeviceName_vi);
             txPcm = NULL;
             sAttr.type = PAL_STREAM_LOW_LATENCY;
             sAttr.direction = PAL_AUDIO_INPUT_OUTPUT;
@@ -1746,6 +1745,7 @@ int32_t SpeakerProtection::spkrProtProcessingMode(bool flag)
 err_pcm_open :
     if (txPcm) {
         pcm_close(txPcm);
+        disableDevice(audioRoute, mSndDeviceName_vi);
         txPcm = NULL;
     }
 
