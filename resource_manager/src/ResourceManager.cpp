52dca6479c9128b06d2cc058cd6d016299ce0684
yidongh <quic_yidongh@quicinc.com>
2021-11-30 18:24:25 +0800

    pal: check if stream is locked by rm before enable/disable EC ref
    In register/deregister device, we call setECRef_l to avoid deadlock
    if there's a device switch taking place, because all streams pending
    device switch will be locked before handling device switch. However,
    streams not pending device switch may not be locked, so if we call
    setECRef_l without locking stream mutex, EC ref enable/disable may
    not getting handled properly because Tx stream may handling other
    operations with stream mutex locked.
    
    Check if stream is locked by rm before enable/disable EC ref.
    
    Change-Id: Ib39e77aaffa5bfc2a50936d4b945b09a55e5a7fe

--- resource_manager/src/ResourceManager.cpp
+++ resource_manager/src/ResourceManager.cpp
@@ -3059,7 +3059,7 @@ int ResourceManager::registerDevice(std::shared_ptr<Device> d, Stream *s)
                     /* For Device switch, stream mutex will be already acquired,
                      * so call setECRef_l instead of setECRef.
                      */
-                    if (isDeviceSwitch)
+                    if (isDeviceSwitch && str->isMutexLockedbyRm())
                         status = str->setECRef_l(device, true);
                     else
                         status = str->setECRef(device, true);
@@ -3111,7 +3111,7 @@ int ResourceManager::registerDevice(std::shared_ptr<Device> d, Stream *s)
                     PAL_DBG(LOG_TAG, "EC ref already set");
                 } else if (str && isStreamActive(str, mActiveStreams)) {
                     mResourceManagerMutex.unlock();
-                    if (isDeviceSwitch)
+                    if (isDeviceSwitch && str->isMutexLockedbyRm())
                         status = str->setECRef_l(d, true);
                     else
                         status = str->setECRef(d, true);
@@ -3211,7 +3211,7 @@ int ResourceManager::deregisterDevice(std::shared_ptr<Device> d, Stream *s)
                     PAL_DBG(LOG_TAG, "EC ref still active, no need to reset");
                 } else if (str && isStreamActive(str, mActiveStreams)) {
                     mResourceManagerMutex.unlock();
-                    if (isDeviceSwitch)
+                    if (isDeviceSwitch && str->isMutexLockedbyRm())
                         status = str->setECRef_l(d, false);
                     else
                         status = str->setECRef(d, false);
@@ -3242,7 +3242,7 @@ int ResourceManager::deregisterDevice(std::shared_ptr<Device> d, Stream *s)
                     PAL_DBG(LOG_TAG, "EC ref still active, no need to reset");
                 } else if (str && isStreamActive(str, mActiveStreams)) {
                     mResourceManagerMutex.unlock();
-                    if (isDeviceSwitch)
+                    if (isDeviceSwitch && str->isMutexLockedbyRm())
                         status = str->setECRef_l(device, false);
                     else
                         status = str->setECRef(device, false);
