3789d25cd5790f0b18fc195e9f2f3a0f7820a16a
Ankit Mishra <quic_ankmis@quicinc.com>
2022-06-24 11:17:54 +0530

    PAL: Aquire wakelock for start and stop
    While start and stop if wakelock is released device may go to deep sleep state,
    which may cause delay in operations, which will cause multiple issue, including
    timeout crashes and sleep monitor issues.
    
    Now, we acquire waklocks for whole cycles of Start and Stop recognition.
    
    Change-Id: I7236263e2f92eb58d40c67109f29ab0e208bb30c

--- session/src/SoundTriggerEngineGsl.cpp
+++ session/src/SoundTriggerEngineGsl.cpp
@@ -1473,7 +1473,8 @@ int32_t SoundTriggerEngineGsl::ProcessStartRecognition(Stream *s) {
     struct pal_mmap_position mmap_pos;
 
     PAL_DBG(LOG_TAG, "Enter");
-
+    std::shared_ptr<ResourceManager> rm = ResourceManager::getInstance();
+    rm->acquireWakeLock();
     // release custom detection event before start
     if (custom_detection_event) {
         free(custom_detection_event);
@@ -1541,6 +1542,7 @@ int32_t SoundTriggerEngineGsl::ProcessStartRecognition(Stream *s) {
     exit_buffering_ = false;
     UpdateState(ENG_ACTIVE);
 exit:
+    rm->releaseWakeLock();
     PAL_DBG(LOG_TAG, "Exit, status %d", status);
     return status;
 }
@@ -1686,6 +1688,8 @@ int32_t SoundTriggerEngineGsl::ProcessStopRecognition(Stream *s) {
     int32_t status = 0;
 
     PAL_DBG(LOG_TAG, "Enter");
+    std::shared_ptr<ResourceManager> rm = ResourceManager::getInstance();
+    rm->acquireWakeLock();
     if (buffer_) {
         buffer_->reset();
     }
@@ -1706,6 +1710,7 @@ int32_t SoundTriggerEngineGsl::ProcessStopRecognition(Stream *s) {
         PAL_ERR(LOG_TAG, "Failed to stop session, status = %d", status);
     }
     UpdateState(ENG_LOADED);
+    rm->releaseWakeLock();
     PAL_DBG(LOG_TAG, "Exit, status = %d", status);
     return status;
 }
