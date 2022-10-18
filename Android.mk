25a4ca4feefa901254d33581108dfbbe2904329b
SGCMarkus <markusornik@gmail.com>
2022-10-18 12:26:51 +0200

    pal: skip libar-pal on OSS builds
    this depends on the proprietary header ar_osal_mem_op.h
    
    Change-Id: I2010f8bb85ef30b3a5eb1044fca3b4f54e1ba10a

--- Android.mk
+++ Android.mk
@@ -4,6 +4,8 @@ ifeq ($(TARGET_USES_QCOM_MM_AUDIO), true)
 LOCAL_PATH := $(call my-dir)
 PAL_BASE_PATH := $(call my-dir)
 
+ifneq ($(QCPATH),)
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE        := libar-pal
@@ -140,6 +142,8 @@ LOCAL_SHARED_LIBRARIES += libtinyalsa libtinycompress
 endif
 
 include $(BUILD_SHARED_LIBRARY)
+
+endif
 
 #-------------------------------------------
 #            Build CHARGER_LISTENER LIB
