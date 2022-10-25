4df1f33324d207015b0513960f70897eb6427bdc
Ankit Mishra <quic_ankmis@quicinc.com>
2022-10-25 11:39:21 +0530

    PAL: Properly update Rx device for EC
    If setting EC for some Rx device becomes unsuccessful, then we should not
    update the current EC ref device with this Rx device, it should have same
    value which it has before this operation.
    
    Now, checking return status before updating the current Rx EC device.
    
    Change-Id: I3ad488d5c4ed0c862d48f9a9e387b97c98f7ea60

--- session/inc/SoundTriggerEngineGsl.h
+++ session/inc/SoundTriggerEngineGsl.h
@@ -204,6 +204,7 @@ class SoundTriggerEngineGsl : public SoundTriggerEngine {
                                                      deregister_config_;
 
     bool is_qcva_uuid_;
+    bool is_crr_dev_using_ext_ec_;
     uint32_t lpi_miid_;
     uint32_t nlpi_miid_;
     bool use_lpi_;
