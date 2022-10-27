c785e746c30bb731330593ae07419aa6975ec5b4
Faiz Nabi Kuchay <quic_fkuchay@quicinc.com>
2022-10-27 18:00:08 +0530

    pal: notify speaker protection processing mode thread
    notify speaker protection processing mode thread when event from dsp is
    not received for calibration thread.
    This will solve race condition where event from dsp is received after
    notify_all in processing mode, which will set the condition to notify the
    thread waiting in processing mode to false.
    
    Change-Id: I38a37f2c1ef284a0bcc1434910242d4081a653b3

--- device/src/SpeakerProtection.cpp
+++ device/src/SpeakerProtection.cpp
@@ -1181,8 +1181,8 @@ exit:
         PAL_DBG(LOG_TAG, "Unlocked due to processing mode");
         spkrCalState = SPKR_NOT_CALIBRATED;
         clock_gettime(CLOCK_BOOTTIME, &spkrLastTimeUsed);
-        cv.notify_all();
     }
+    cv.notify_all();
 
     if (ret != 0) {
         // Error happened. Reset timer
